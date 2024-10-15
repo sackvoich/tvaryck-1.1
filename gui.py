f3942eb276fcad34b35e79c0b348c5acc6803d85
Saveliy <kurashov2015@yandex.by>
2024-10-15 22:19:08 +0300

    Comments updated

--- gui.py
+++ gui.py
@@ -1,5 +1,3 @@
-# gui.py
-
 import sys
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QComboBox,
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 import cv2
 import numpy as np
 import face_recognition
